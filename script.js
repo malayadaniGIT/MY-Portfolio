021f6c55ebb7768ce713309710f20e282df4a584
malayadaniGIT <malayadani@gmail.com>
2024-02-01 02:59:46 +0530

    Portfolio update

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Developer", "Programmer", "Web Designer", "Freelancer"],
+        strings: ["Developer", "Programmer", "Web Designer", ],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
